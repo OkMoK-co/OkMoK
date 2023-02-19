0319c0b70a9553925feaf537ce76e7a3c85a63a3
KimDae-hyun <dhkim5608@naver.com>
2023-02-19 11:59:02 +0900

    [Feat] 메인 페이지 뷰 만들기 #4

---
+++ hybrid/src/components/main/MakeRoom.tsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/main/Main.module.scss';
+
+export default function MakeRoom() {
+  return (
+    <div className={styles.makeRoom}>
+      <input type='button' value='🚀 M a k e' />
+    </div>
+  );
+}
