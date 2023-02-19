228f2cd325f9f48d5bdabe02e67891166f40cf9c
pearpearB <smilyoung@gmail.com>
2023-02-19 13:47:00 +0900

    [Feat] 헤더, 푸터 구현 #2

---
+++ hybrid/src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div>github</div>
+      <div>이용약관</div>
+      <div>개인정보처리방침</div>
+    </footer>
+  );
+}
---
+++ hybrid/src/components/layout/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <div>OkMoK</div>
+      <div></div>
+    </header>
+  );
+}
