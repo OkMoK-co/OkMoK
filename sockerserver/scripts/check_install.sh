87325d648b3c5f0494b5ab2b131090cd40d36c2d
donghyuk <reg0145@naver.com>
2023-03-16 11:49:02 +0900

    [Fix] ruby -> bash 변경 #10

--- sockerserver/scripts/check_install.sh
+++ sockerserver/scripts/check_install.sh
@@ -7,7 +7,7 @@ if [[ "$(uname)" == "Darwin" ]]; then
 		echo "✅ Brew installed"
 	else
 		echo "Brew is not installed. Installing Brew..."
-		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	fi
 
 	# Check install Poco_lib By MacOS
