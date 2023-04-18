e27c7185a38abc359e44576285f1844935a24938
pearpearB <smilyoung@gmail.com>
2023-04-18 12:13:01 +0900

    [Style] 메인페이지 레이아웃 및 전체적인 페이지 스타일 선언 및 적용 #45

--- hybrid/src/components/layout/Header.tsx
+++ hybrid/src/components/layout/Header.tsx
@@ -1,7 +1,52 @@
+import styled from 'styled-components';
+
 export default function Header() {
   return (
-    <header>
-      <h1>OkMoK</h1>
-    </header>
+    <Container>
+      <h1>
+        <span>O</span>
+        <span>k</span>
+        <span>M</span>
+        <span>o</span>
+        <span>K</span>
+      </h1>
+    </Container>
   );
 }
+
+const Container = styled.header`
+  ${({ theme }) => theme.flexs.center};
+  height: 5rem;
+  margin: 1.5rem;
+  h1 {
+    font-size: ${({ theme }) => theme.fontsizes.xxlarge};
+    span {
+      margin: 0 0.2rem;
+    }
+    span:nth-child(1) {
+      color: ${({ theme }) => theme.colors.red};
+    }
+    span:nth-child(2) {
+      color: ${({ theme }) => theme.colors.green};
+    }
+    span:nth-child(3) {
+      color: ${({ theme }) => theme.colors.blue};
+    }
+    span:nth-child(4) {
+      color: ${({ theme }) => theme.colors.yellow};
+    }
+    span:nth-child(5) {
+      color: ${({ theme }) => theme.colors.aqua};
+    }
+  }
+  @media (${({ theme: { device } }) => device.mobile}) {
+    height: 2.5rem;
+    margin: 0.8rem;
+    h1 {
+      font-size: ${({ theme }) => theme.fontsizes.xlarge};
+      span {
+        margin: 0 0.1rem;
+      }
+    }
+  }
+`;
