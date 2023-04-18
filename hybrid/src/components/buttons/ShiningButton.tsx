bcd9515a1df1e23c53904c6d32e8c61f85b71c2a
donghyuk <reg0145@naver.com>
2023-04-18 19:55:25 +0900

    [Feat] 404 페이지 구현 close #54

---
+++ hybrid/src/components/buttons/ShiningButton.tsx
@@ -0,0 +1,131 @@
+import styled from 'styled-components';
+
+interface ShinningButtonProps {
+  value: any;
+  clickHandler: () => void;
+}
+
+export default function ShiningButton({
+  value,
+  clickHandler,
+}: ShinningButtonProps) {
+  return (
+    <ShinningEffect>
+      <a onClick={clickHandler}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        {value}
+      </a>
+    </ShinningEffect>
+  );
+}
+
+const ShinningEffect = styled.div`
+  a {
+    color: #fefefe;
+    text-decoration: none;
+    text-transform: uppercase;
+    text-shadow: 0 0 0.5rem #fefefe;
+    transition: 0.5s;
+    overflow: hidden;
+    position: relative;
+    display: inline-block;
+    padding: 30px 50px;
+    margin: 90px 50;
+  }
+  a:hover {
+    background: #fefefe;
+    color: #050801;
+    box-shadow: 0 0 5px #03e9f4, 0 0 25px #03e9f4, 0 0 50px #03e9f4,
+      0 0 200px #03e9f4;
+    -webkit-box-reflect: below 1px linear-gradient(transition, #0005);
+  }
+  a:nth-child(2) {
+    filter: hue-rotate(270deg);
+  }
+  a span {
+    position: absolute;
+    display: block;
+  }
+  a span:nth-child(1) {
+    top: 0;
+    background: linear-gradient(90deg, transparent, #ff0000);
+    animation: top-animate 2s linear infinite;
+    left: 0;
+    width: 100%;
+    height: 2px;
+  }
+
+  a span:nth-child(2) {
+    top: -100%;
+    background: linear-gradient(180deg, transparent, #00ff00);
+    animation: right-animate 1.5s linear infinite;
+    right: 0;
+    width: 2px;
+    height: 100%;
+    animation-delay: 0.25s;
+  }
+
+  /* 3rd line animation*/
+  a span:nth-child(3) {
+    bottom: 0;
+    right: 0;
+    background: linear-gradient(270deg, transparent, #0000ff);
+    animation: bottom-animate 2s linear infinite;
+    width: 100%;
+    height: 2px;
+  }
+
+  /* 4th line animation*/
+  a span:nth-child(4) {
+    bottom: -100%;
+    left: 0;
+    background: linear-gradient(360deg, transparent, #ffff00);
+    animation: left-animate 1.5s linear infinite;
+    width: 2px;
+    height: 100%;
+    animation-delay: 0.75s;
+  }
+
+  @keyframes top-animate {
+    0% {
+      left: -100%;
+    }
+    50%,
+    100% {
+      left: 100%;
+    }
+  }
+
+  @keyframes right-animate {
+    0% {
+      top: -100%;
+    }
+    50%,
+    100% {
+      top: 100%;
+    }
+  }
+
+  @keyframes bottom-animate {
+    0% {
+      right: -100%;
+    }
+    50%,
+    100% {
+      right: 100%;
+    }
+  }
+
+  @keyframes left-animate {
+    0% {
+      bottom: -100%;
+    }
+    50%,
+    100% {
+      bottom: 100%;
+    }
+  }
+`;
---
+++ hybrid/src/pages/404.tsx
@@ -0,0 +1,48 @@
+import { ContentContainer } from '@/styles/common-style';
+import { theme } from '@/styles/theme';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import ShiningBackground from '@/components/animation/ShiningBackground';
+import ShiningButton from '@/components/buttons/ShiningButton';
+
+export default function Page404() {
+  const router = useRouter();
+
+  const GoHomeHandler = () => {
+    router.push('/login');
+  };
+
+  return (
+    <Container>
+      <ShiningBackground />
+      <Content>
+        <h1> 404 </h1>
+        <p> Page Not Found </p>
+        <ShiningButton value='Go Home' clickHandler={GoHomeHandler} />
+      </Content>
+    </Container>
+  );
+}
+
+const Container = styled(ContentContainer)`
+  ${({ theme }) => theme.flexs.center}
+`;
+
+const Content = styled.div`
+  ${theme.flexs.centerColumn};
+  h1 {
+    font-size: 9em;
+    line-height: 1.3em;
+    margin: 0 auto;
+    padding: 0;
+    text-shadow: 0 0 1rem #fefefe;
+  }
+  p {
+    max-width: 480px;
+    font-size: 1.5em;
+    line-height: 1.15em;
+    padding: 0 1rem;
+    margin: 0 1rem 2rem 1rem;
+    text-shadow: 0 0 0.5rem #fefefe;
+  }
+`;
